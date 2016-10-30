5dde8c94868122f789342e71f62f991b0d264d08
wester <wester@cform.io>
2016-10-31 03:36:21 +0800

    Styles part2.4

--- tests/dummy/app/modules/question-picture-choice-matrix/route.js
+++ tests/dummy/app/modules/question-picture-choice-matrix/route.js
@@ -9,6 +9,95 @@ export default Route.extend({
     return {
       nodes: [
         {
+          title: 'Picture Matrix insert-block',
+          description: faker.lorem.paragraph(),
+          images: '',
+          renderId:'775519',
+          typeName: '选择题',
+          quesType: 'picture-choice', //select, fill
+          selectType: 'radio',
+          showStyle: 'matrix',
+          textDirection:'insert-block', //in-block ,out-block, empty
+          pictureSize: 'picture-large',  //picture-large, picture-small
+          uuid: faker.date.between('2016-01-01', '2016-12-31'),
+          isMust:false,
+          number:'2',
+          options:[
+            Ember.Object.create({
+              selected: false,
+              renderId: '4567890-0987',
+              text: '',
+              uuid: faker.date.between('2016-01-01', '2016-12-31'),
+              icon: 'radio', // 选项的Icon
+              image: '/images/sample-1.jpg',
+              inputType: '', // 'select', 'input', 'select-input, ower-input',
+              inputRule: '', //输入控件初始化规则
+              value: '',
+              placeholder: '',
+            }),
+            Ember.Object.create({
+              selected: false,
+              renderId: '4567890-0987',
+              text: '',
+              uuid: faker.date.between('2016-01-01', '2016-12-31'),
+              icon: 'radio', // 选项的Icon
+              image: '/images/sample-2.jpg',
+              inputType: '', // 'select', 'input', 'select-input, ower-input',
+              inputRule: '', //输入控件初始化规则
+              value: '',
+              placeholder: '',
+            }),
+            Ember.Object.create({
+              selected: false,
+              renderId: '4567890-0987',
+              text: '',
+              uuid: faker.date.between('2016-01-01', '2016-12-31'),
+              icon: 'radio', // 选项的Icon
+              image: '/images/sample-3.jpg',
+              inputType: '', // 'select', 'input', 'select-input, ower-input',
+              inputRule: '', //输入控件初始化规则
+              value: '',
+              placeholder: '',
+            }),
+            Ember.Object.create({
+              selected: false,
+              renderId: '4567890-0987',
+              text: '',
+              uuid: faker.date.between('2016-01-01', '2016-12-31'),
+              icon: 'radio', // 选项的Icon
+              image: '/images/sample-3.jpg',
+              inputType: '', // 'select', 'input', 'select-input, ower-input',
+              inputRule: '', //输入控件初始化规则
+              value: '',
+              placeholder: '',
+            }),
+            Ember.Object.create({
+              selected: false,
+              renderId: '4567890-0987',
+              text: '',
+              uuid: faker.date.between('2016-01-01', '2016-12-31'),
+              icon: 'radio', // 选项的Icon
+              image: '/images/sample-4.jpg',
+              inputType: '', // 'select', 'input', 'select-input, ower-input',
+              inputRule: '', //输入控件初始化规则
+              value: '',
+              placeholder: '',
+            }),
+            Ember.Object.create({
+              selected: false,
+              renderId: '4567890-0987',
+              text: '',
+              uuid: faker.date.between('2016-01-01', '2016-12-31'),
+              icon: 'radio', // 选项的Icon
+              image: '/images/sample-5.jpg',
+              inputType: '', // 'select', 'input', 'select-input, ower-input',
+              inputRule: '', //输入控件初始化规则
+              value: '',
+              placeholder: '',
+            })
+          ],
+        },
+        {
           title: 'Picture Matrix',
           description: faker.lorem.paragraph(),
           images: '',
