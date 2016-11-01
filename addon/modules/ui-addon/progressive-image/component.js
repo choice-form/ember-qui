5707f643bc3525deaf3fbca56fb79e4e82805ae4
余凡 <albert@cform.io>
2016-11-02 02:30:35 +0800

    Use canvas to draw blur effect to progressive-image

--- addon/modules/ui-addon/progressive-image/component.js
+++ addon/modules/ui-addon/progressive-image/component.js
@@ -3,6 +3,8 @@ import layout from './template';
 import computed from 'ember-computed';
 import get from 'ember-metal/get';
 import { htmlSafe } from 'ember-string';
+import stackBlurImage from './stack-blur';
+import { later } from 'ember-runloop';
 
 export default Component.extend({
   layout,
@@ -17,16 +19,31 @@ export default Component.extend({
   didInsertElement() {
     this._super(...arguments);
 
+    const canvas = document.createElement('canvas');
+    canvas.id = 'stack-blur-canvas';
+
     const thumbnail = new Image();
     thumbnail.classList.add('thumbnail');
     thumbnail.src = get(this, 'thumbnail');
-    thumbnail.onload = () => thumbnail.classList.add('loaded');
+    thumbnail.onload = () => {
+      thumbnail.classList.add('loaded');
+      stackBlurImage(thumbnail, 'stack-blur-canvas', 5);
+    }
     this.element.appendChild(thumbnail);
+    this.element.appendChild(canvas);
 
     const image = new Image();
     image.classList.add('image');
     image.src = get(this, 'image');
-    image.onload = () => image.classList.add('loaded');
+    image.onload = () => {
+      image.classList.add('loaded');
+      later(this, 'teardownStackBlueEffect', canvas, thumbnail, 1000);
+    };
     this.element.appendChild(image);
+  },
+
+  teardownStackBlueEffect(canvas, thumbnail) {
+    this.element.removeChild(canvas);
+    this.element.removeChild(thumbnail);
   }
 });
