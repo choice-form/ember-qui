284724df64f680dec7de12b562bcc29bd3388a07
wester <wester@cform.io>
2016-11-03 00:20:39 +0800

    Styles part3.9

--- tests/dummy/app/modules/question-end-page/route.js
+++ tests/dummy/app/modules/question-end-page/route.js
@@ -11,7 +11,7 @@ export default Route.extend({
     return {
       nodes:[
         {
-          title: faker.lorem.words(),
+          title: faker.finance.accountName(),
           description: faker.lorem.paragraph(),
           images: [
             {
