d213e3e09810085e5ced8c8ce4bf92fd58390c93
shengxx <janisesheng@cform.io>
2016-11-06 01:16:41 +0800

    抽奖，模拟中奖 跳转到微信页面

--- addon/modules/ui-reward/component.js
+++ addon/modules/ui-reward/component.js
@@ -1,6 +1,7 @@
 import Component from 'ember-component';
 import layout from './template';
 import set from 'ember-metal/set';
+import get from 'ember-metal/get';
 
 export default Component.extend({
   layout,
@@ -8,6 +9,11 @@ export default Component.extend({
   actions: {
     handleClick() {
       set(this, 'currency', true)
+    },
+
+    handleNextClick(){
+      this.node.handleEvents.handleNextClick(get(this,'node'));
     }
+
   }
 }).reopenClass({ positionalParams: ['node'] });
