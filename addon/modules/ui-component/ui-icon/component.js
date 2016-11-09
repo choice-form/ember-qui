911648e4e5b0d85a5ef0d0ee4a638b21bb0aa48f
shengxx <janisesheng@cform.io>
2016-11-09 15:01:14 +0800

    ui-icon

--- addon/modules/ui-component/ui-icon/component.js
+++ addon/modules/ui-component/ui-icon/component.js
@@ -1,11 +1,10 @@
 import Component from 'ember-component';
 import layout from './template';
 
-export default Component.extend({
 
+export default Component.extend({
   layout,
   tagName:'',
-  size: '16px',
-  viewBox: '16',
 
-});
+
+}).reopenClass({ positionalParams: ['node', 'option']});
