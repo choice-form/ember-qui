05612fb88849f52c8f9f6e809ce782ed6443f115
shengxx <janisesheng@cform.io>
2016-11-11 16:19:30 +0800

    拆分 ui-dropdown

--- addon/modules/ui-question/dropdown/component.js
+++ addon/modules/ui-question/dropdown/component.js
@@ -1,9 +1,33 @@
 import Component from 'ember-component';
 import layout from './template';
+import get from 'ember-metal/get';
+import computed from 'ember-computed';
+import {mobiInitTreeList} from '../../lib/mobile-factory'
+
 
 export default Component.extend({
   layout,
-  tagName: '',
+  classNames:['dropdown-layout'],
+  attributeBindings:['data-render-id'],
+  'data-render-id': computed.oneWay('node.renderId'),
 
+  didRender(){
+    const input = this.element.getElementsByClassName('dropdown-list')[0];
+    mobiInitTreeList(input, {
+      placeholder: get(this, 'node.placeholder'),
+
+      /* eslint-disable no-unused-vars*/
+      onSet: (event, inst)=>{
+        this.handleEvents.handleQuestionInput(event.valueText, get(this,'node'));
+        input.previousElementSibling.value = get(this, 'node.value');
+      },
+      onInit: () => {
+        const mobiInput = input.previousElementSibling;
+        mobiInput.value = get(this, 'node.value');
+        //设置input的class名称
+        mobiInput.setAttribute('class', 'ui-menu');
+      }
+    });
+  }
 
 }).reopenClass({ positionalParams: ['node','handleEvents']});
