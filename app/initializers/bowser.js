66e199564ed1b833778b4b2517104f90989f08f5
余凡 <albert@cform.io>
2016-11-13 22:52:34 +0800

    Fix browser detection under wechat and ie

--- app/initializers/bowser.js
+++ app/initializers/bowser.js
@@ -4,12 +4,15 @@ export default {
   name: 'bowser',
 
   initialize() {
-    if (bowser.msie && bowser.version <= 9) {
+    if (bowser.msie) {
       const classNames = ` ie -${bowser.version.split('.')[0]}`
       document.documentElement.classNames += classNames
     } else {
       const classNames = bowser.name.toLowerCase().split(' ')
-      classNames.push('-' + bowser.version.split('.')[0])
+
+      if (bowser.version) {
+        classNames.push('-' + bowser.version.split('.')[0])
+      }
 
       const classList = document.documentElement.classList
       classList.add.apply(classList, classNames)
