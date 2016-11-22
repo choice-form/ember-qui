30a5b745fb38760a9cc6feea8baa27933fb32dee
fenyiwudian <laiq@cform.io>
2016-11-22 23:15:58 +0800

    check newMasonry present

--- addon/modules/ui-question/picture-choice/component.js
+++ addon/modules/ui-question/picture-choice/component.js
@@ -3,19 +3,18 @@ import layout from './template';
 import get from 'ember-metal/get';
 import computed from 'ember-computed';
 import Masonry from 'masonry';
-import imagesLoaded from 'imagesloaded';
 
 
 export default Component.extend({
   layout,
-  classNames:['picture-layout'],
+  classNames: ['picture-layout'],
   classNameBindings: ['class_names'],
   class_names: computed('node.showStyle', 'node.picStyle', 'node.textDirection', 'node.pictureSize', function () {
     const showStyle = get(this, 'node.showStyle');
     const picStyle = get(this, 'node.picStyle');
     const textDirection = get(this, "node.textDirection");
     const pictureSize = get(this, "node.pictureSize");
-    const _className = `${showStyle ? showStyle : null}`+` ${picStyle ? picStyle : null}`+` ${textDirection ? textDirection : null}`+` ${pictureSize ? pictureSize : null}`;
+    const _className = `${showStyle ? showStyle : null}` + ` ${picStyle ? picStyle : null}` + ` ${textDirection ? textDirection : null}` + ` ${pictureSize ? pictureSize : null}`;
     return _className;
   }),
 
@@ -24,19 +23,19 @@ export default Component.extend({
     /**
      * click事件
      */
-    handleOptionClick(option,e){
+    handleOptionClick(option, e){
       !this.handleEvents.handleOptionClick(option, get(this, 'node'))
       && e.preventDefault();
     },
   },
 
-  didRender(){
-    const showStyle = get(this, 'node.showStyle');
+  didInsertElement() {
+    if (this.element) {
+      const showStyle = get(this, 'node.showStyle');
 
-    if(showStyle == 'pinterest'){
-      imagesLoaded(this.element, () => {
+      if (showStyle == 'pinterest') {
         this.newMasonry = new Masonry(this.element);
-      });
+      }
     }
   },
 
