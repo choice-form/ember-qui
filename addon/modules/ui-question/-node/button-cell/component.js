bc176086fb1cf8213052d9b38249f7abfda2b2a0
fenyiwudian <laiq@cform.io>
2016-11-22 23:02:39 +0800

    remove one-by-one

--- addon/modules/ui-question/-node/button-cell/component.js
+++ addon/modules/ui-question/-node/button-cell/component.js
@@ -2,7 +2,6 @@ import Component from 'ember-component';
 import layout from './template';
 import get from 'ember-metal/get';
 
-let num = 1;
 export default Component.extend({
   layout,
 
@@ -14,15 +13,7 @@ export default Component.extend({
     },
 
     handleNextClick() {
-      num ++ ;
-      const article =  document.getElementsByTagName('article')[0];
-      if(num%2 == 0){
-        article.setAttribute('class', 'choiceform one-by-one');
-      }else{
-        article.setAttribute('class', 'choiceform');
-      }
-
       get(this, 'handleNextClick')();
-    },
+    }
   }
 });
