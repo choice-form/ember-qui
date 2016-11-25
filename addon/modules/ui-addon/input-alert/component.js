dc90e4b0b7e0c724038fe8dfb9629b87a745dd27
shengxx <janisesheng@cform.io>
2016-11-25 14:24:23 +0800

    清楚eslint error

--- addon/modules/ui-addon/input-alert/component.js
+++ addon/modules/ui-addon/input-alert/component.js
@@ -1,8 +1,5 @@
 import Component from 'ember-component';
 import layout from './template';
-import {htmlSafe} from 'ember-string';
-import set from 'ember-metal/set';
-import {later} from 'ember-runloop';
 
 export default Component.extend({
   tagName: '',
