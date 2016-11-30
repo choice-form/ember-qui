eb0f6224a656d37cde7d5d40275adb20cc432fb1
余凡 <albert@cform.io>
2016-12-01 05:42:30 +0800

    #5 Performance Improvements

--- addon/modules/ui-question/ranking/component.js
+++ addon/modules/ui-question/ranking/component.js
@@ -2,128 +2,125 @@ import Component from 'ember-component';
 import layout from './template';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
-import computed, {reads} from 'ember-computed';
+import { reads } from 'ember-computed';
 import Sortable from 'sortable';
-import {scheduleOnce, later} from 'ember-runloop';
-import {addClass, removeClass} from '../../lib/attribute-manage'
+import { scheduleOnce, later } from 'ember-runloop';
+import { addClass, removeClass } from '../../lib/attribute-manage'
 import $ from 'jquery';
 import device from 'device';
 
 export default Component.extend({
   layout,
 
-  classNames:['ui-ranking'],
-  attributeBindings:['data-render-id'],
+  classNames: ['ui-ranking'],
+  attributeBindings: ['data-render-id'],
   'data-render-id': reads('node.renderId'),
-
-  isScrollUp: true,
-  isScrollDown: true,
-
-  isDesktop: computed(function () {
-    return device.desktop();
-  }),
 
   actions: {
     handleOptionClick(){
-      this.handleEvents.handleOptionClick(get(this, 'option'),get(this,'node'));
+      this.handleEvents.handleOptionClick(get(this, 'option'), get(this,'node'));
     },
 
     handleOptionInput(e){
-      const value = e.target.value;
-      set(this, 'option.value', value);
-      this.handleEvents.handleOptionInput(get(this, 'option'),get(this,'node'));
+      set(this, 'option.value', e.target.value);
+      this.handleEvents.handleOptionInput(get(this, 'option'), get(this,'node'));
     },
   },
 
   renderSortable() {
-    //初始化
     const options = get(this, 'options');
-    options.forEach((item,index)=>{
-      if(item.sortNo > 0){
-        addClass(this.element.getElementsByClassName('ranking-rank ')[index], 'complete')
+    const rankItems = this.element.querySelectorAll('.ranking-rank');
+    options.forEach((item, index)=>{
+      if (item.sortNo > 0) {
+        addClass(rankItems[index], 'complete')
       }
     });
 
-    //sortable事件
-    this.sortTable = new Sortable(this.element, {
+    this.sortable = new Sortable(this.element, {
       handle: '.handle',
       scroll: false,
-      scrollSensitivity: get(this, 'isDesktop') ? 64 : 0,
+      scrollSensitivity: device.desktop() ? 64 : 0,
       animation: 250,
       sort: true,
       ghostClass: "ghost",
-      onEnd: (event)=> {
-        const {oldIndex} = event;
-        let {newIndex} = event;
+      onEnd: event => {
+        let { oldIndex, newIndex } = event;
+
         if (newIndex === undefined) {
           newIndex = oldIndex;
         }
-        const indexArray = this.handleEvents.handleOptionDrop(oldIndex, newIndex, get(this,'node'));
 
-        indexArray.forEach((item)=>{
-          const sortNo = parseInt(item);
-          const index = item -1;
-          const thisNode = this.element.getElementsByClassName('ranking-rank ')[index];
-          thisNode.querySelector('.ranking-number').innerHTML=sortNo;
-          removeClass(thisNode, 'complete');
-          addClass(thisNode, 'complete event');
+        const indexArray = this.handleEvents.handleOptionDrop(
+          oldIndex, newIndex, get(this, 'node')
+        );
+
+        indexArray.forEach(item => {
+          const sortNo = window.parseInt(item, 10);
+          const index = item - 1;
+          const rankItem = rankItems[index];
+          rankItem.querySelector('.ranking-number').innerHTML = sortNo;
+          removeClass(rankItem, 'complete');
+          addClass(rankItem, 'complete event');
           later(()=>{
-            removeClass(thisNode, 'event');
-          },1000);
+            removeClass(rankItem, 'event');
+          }, 1000);
         });
       }
     });
   },
 
-  scrollMovie(event){
+  scrollMovie(event) {
+    if (event.target.className !== 'handle') return ;
 
-    if(event.target.className !== 'handle') return ;
     const offset = 64;
     const clientY = event.targetTouches[0].clientY;
     const winowHeight = $(window).height();
     const offsetX = offset;
     const offsetY = winowHeight - offset;
 
-    const scrollAnimate = (num)=>{
+    const scrollAnimate = num => {
       const scrollTop = $('body').scrollTop;
       const scrollTime = scrollTop * 2 / winowHeight > 0.3 ? 4000 : 2000;
-      $('body').animate({scrollTop: num}, scrollTime);
+      $('body').animate({ scrollTop: num }, scrollTime);
     };
 
-    if((clientY < offsetX) && this.isScrollUp){
+    if ((clientY < offsetX) && this.isScrollUp) {
       scrollAnimate(0);
       this.isScrollUp = false;
     }
 
-    if((clientY > offsetY) && this.isScrollDown){
+    if ((clientY > offsetY) && this.isScrollDown) {
       scrollAnimate(winowHeight);
       this.isScrollDown = false;
     }
 
-    if((clientY <= offsetY) && (clientY >= offsetX) && (!this.isScrollUp || !this.isScrollDown)){
+    if ((clientY <= offsetY) && (clientY >= offsetX)
+        && (!this.isScrollUp || !this.isScrollDown)) {
       $('body').stop();
       this.isScrollUp = true;
       this.isScrollDown = true;
     }
   },
 
-  scrollStop(){
+  scrollStop() {
     $('body').stop();
   },
 
-
   didInsertElement(){
     scheduleOnce('afterRender', this, 'renderSortable');
-    if(get(this, 'isDesktop')) return ;
-    this.element.addEventListener('touchmove',this.scrollMovie,false);
-    this.element.addEventListener('touchend',this.scrollStop,false);
+
+    if (device.desktop()) return;
+
+    this.element.addEventListener('touchmove', this.scrollMovie, false);
+    this.element.addEventListener('touchend', this.scrollStop, false);
   },
 
   didDestroyElement(){
-    this.sortTable.destroy();
-    if(get(this, 'isDesktop')) return ;
-    this.element.removeEventListener('touchmove',this.scrollMovie,false);
-    this.element.removeEventListener('touchend',this.scrollStop,false);
+    this.sortable && this.sortable.destroy();
 
+    if (device.desktop()) return ;
+
+    this.element.removeEventListener('touchmove', this.scrollMovie, false);
+    this.element.removeEventListener('touchend', this.scrollStop, false);
   }
 }).reopenClass({ positionalParams: ['node', 'handleEvents']});
