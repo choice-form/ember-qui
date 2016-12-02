07c62587a1c8b7315d4ad1733a6cbbcfd8e8cf6a
Albert Yu <dev.yufan@gmail.com>
2016-12-02 18:05:28 +0800

    ref #3 Optimize ui-question/dropdown

--- addon/modules/ui-question/dropdown/component.js
+++ addon/modules/ui-question/dropdown/component.js
@@ -1,9 +1,8 @@
 import Component from 'ember-component';
 import layout from './template';
 import get from 'ember-metal/get';
-import {reads} from 'ember-computed';
-import {mobiInitTreeList} from '../../lib/mobile-factory'
-
+import { reads } from 'ember-computed';
+import { mobiInitTreeList } from '../../lib/mobile-factory';
 
 export default Component.extend({
   layout,
@@ -13,10 +12,10 @@ export default Component.extend({
 
   didRender(){
     const input = this.element.getElementsByClassName('dropdown-list')[0];
+
     mobiInitTreeList(input, {
       placeholder: get(this, 'node.placeholder'),
 
-      /* eslint-disable no-unused-vars*/
       onSet: (event, inst)=>{
         this.handleEvents.handleQuestionInput(event.valueText, get(this,'node'));
         input.previousElementSibling.value = get(this, 'node.value');
@@ -24,10 +23,8 @@ export default Component.extend({
       onInit: () => {
         const mobiInput = input.previousElementSibling;
         mobiInput.value = get(this, 'node.value');
-        //设置input的class名称
         mobiInput.setAttribute('class', 'ui-menu');
       }
     });
   }
-
 }).reopenClass({ positionalParams: ['node','handleEvents']});
