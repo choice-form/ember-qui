4c932d87eaed4073188d9be27f423e4c5209bd71
Albert Yu <dev.yufan@gmail.com>
2016-12-02 16:34:41 +0800

    ref #3 Refactor lazy-loader

--- addon/modules/ui-question/lazy-loader/component.js
+++ addon/modules/ui-question/lazy-loader/component.js
@@ -5,51 +5,45 @@ import computed from 'ember-computed'
 import { htmlSafe } from 'ember-string'
 import get from 'ember-metal/get'
 import set, { setProperties } from 'ember-metal/set'
-import { scheduleOnce } from 'ember-runloop'
 
 export default Component.extend(InViewportMixin, {
   layout,
   classNames: ['lazy-loader'],
   attributeBindings: ['style'],
 
+  viewable: false,
   height: computed(() => 'auto'),
 
   style: computed('height', function() {
     return htmlSafe(`height: ${get(this, 'height')};`)
   }).readOnly(),
 
+  didReceiveAttrs() {
+    if (!get(this, 'isSingle')) set(this, 'viewable', true)
+  },
+
   didInsertElement() {
     this._super(...arguments)
-    this.vh = document.documentElement.clientHeight
-    set(this, 'viewportSpy', get(this, 'isSingle'))
+    setProperties(this, {
+      viewportRefreshRate: 500,
+      viewportSpy: get(this, 'isSingle')
+    })
   },
 
   didEnterViewport() {
     if (!get(this, 'viewable')) set(this, 'viewable', true)
-    scheduleOnce('afterRender', this, 'applyContentHeight')
+    get(this, 'viewportSpy') && set(this, 'height', 'auto')
   },
-
-  applyContentHeight() { set(this, 'height', 'auto') },
 
   didExitViewport() {
     const { height, top, bottom } = this.element.getBoundingClientRect()
+    const viewportTolerance = { top: height, bottom: height, left: 0, right: 0 }
 
     // top below viewport bottom or bottom over viewport top
-    if (top > this.vh || bottom < 0) {
-      setProperties(this, {
-        viewable: false,
-        height: `${height}px`,
-        viewportTolerance: {
-          top: height, bottom: height, left: 0, right: 0
-        }
-      })
+    if (top > document.documentElement.clientHeight || bottom < 0) {
+      setProperties(this, { viewable: false, height: `${height}px`, viewportTolerance })
     } else {
-      setProperties(this, {
-        viewable: true,
-        viewportTolerance: {
-          top: height, bottom: height, left: 0, right: 0
-        }
-      })
+      setProperties(this, { viewable: true, viewportTolerance })
     }
   }
 })
