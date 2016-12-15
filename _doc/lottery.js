752f72415cecd0da3b3ae9ae043cfcdf607ac281
shengxx <janisesheng@cform.io>
2016-12-15 16:10:23 +0800

    拆分 lottery 并更新文档 。 发布版本0.0.50

--- _doc/lottery.js
+++ _doc/lottery.js
@@ -1,12 +1,12 @@
 /**
  * 奖励抽奖页面
  *
- * @class random(奖励抽奖页面)
+ * @class lottery(奖励抽奖页面)
  */
 
 
 /**
- * node for random(奖励抽奖页面)
+ * node for lottery(奖励抽奖页面)
  *
  * @property {object} node
  * @property {String} node.title - 节点标题
@@ -14,12 +14,9 @@
  * @property {Array} node.images - 图片列表
  * @property {String} node.renderId - 页面渲染的ID号,单页滚动时候需要
  * @property {String} node.typeName - 选项类型名称: 抽奖节点
- * @property {String} node.quesType - 当前的题型: random
+ * @property {String} node.quesType - 当前的题型: lottery
  * @property {String} node.uuid - 当前题目节点的唯一ID
- *
- * @property {String} node.rewardName - 奖励名称
- * @property {Bool} node.isReward:, - 抽奖结果 , true, false
- * @property {String} node.buttonText - 如果中奖,会出现此按钮,前去领奖
+ * @property {String} node.renderId - 渲染I
  * @property {String} node.rewardTime - 中奖时间
  * @example
  ```javascript
@@ -34,10 +31,10 @@
       ],
       title: faker.finance.accountName(),
       description: faker.lorem.paragraph(),
+      renderId: faker.date.between('2016-01-01', '2016-12-31'),
       typeName:'抽奖节点',
-      quesType: 'random',
-      isReward:true,
-      buttonText: '前往领取',
+      rewardVaule: `$${faker.finance.mask()}`,
+      quesType: 'lottery',
       uuid: '002',
       rewardTime:'2016-11-11 00:00',
  }
@@ -52,6 +49,6 @@
  * @return {Promise} 会解析处理结果的Promise
 
  handleNextClick(){
-    return new Promise();
+
   }
  */
