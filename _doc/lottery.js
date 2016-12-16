8e1bb8af78a07ef2e293f9567f2c28d1e2b0fe4c
shengxx <janisesheng@cform.io>
2016-12-16 17:05:44 +0800

    抽奖节点，传参规则调整

--- _doc/lottery.js
+++ _doc/lottery.js
@@ -11,12 +11,14 @@
  * @property {object} node
  * @property {String} node.title - 节点标题
  * @property {String} node.description - 节点描述
+ * @property {bool} node.hasReward - 是否中奖
+ * @property {bool} node.rewardNumber - 奖励数量
  * @property {Array} node.images - 图片列表
  * @property {String} node.renderId - 页面渲染的ID号,单页滚动时候需要
  * @property {String} node.typeName - 选项类型名称: 抽奖节点
  * @property {String} node.quesType - 当前的题型: lottery
  * @property {String} node.uuid - 当前题目节点的唯一ID
- * @property {String} node.renderId - 渲染I
+ * @property {String} node.renderId - 渲染
  * @property {String} node.rewardTime - 中奖时间
  * @example
  ```javascript
@@ -33,6 +35,8 @@
       description: faker.lorem.paragraph(),
       renderId: faker.date.between('2016-01-01', '2016-12-31'),
       typeName:'抽奖节点',
+      hasReward:true, //bool值
+      rewardNumber:'100',
       quesType: 'lottery',
       uuid: '002',
       rewardTime:'2016-11-11 00:00',
