25d1a01dc034ad269809fc8e5f7663ee9226de47
shengxx <janisesheng@cform.io>
2016-12-17 15:44:59 +0800

    解决瀑布流小图错位问题

--- addon/modules/ui-question/picture-choice/component.js
+++ addon/modules/ui-question/picture-choice/component.js
@@ -1,21 +1,12 @@
 import Component from 'ember-component';
 import layout from './template';
 import get from 'ember-metal/get';
-import computed from 'ember-computed';
 import Masonry from 'masonry';
+import { scheduleOnce } from 'ember-runloop';
 
 export default Component.extend({
   layout,
-  classNames: ['picture-layout'],
-  classNameBindings: ['class_names'],
-  class_names: computed('node.showStyle', 'node.picStyle', 'node.textDirection', 'node.pictureSize', function () {
-    const showStyle = get(this, 'node.showStyle');
-    const picStyle = get(this, 'node.picStyle');
-    const textDirection = get(this, "node.textDirection");
-    const pictureSize = get(this, "node.pictureSize");
-    const _className = `${showStyle ? showStyle : null}` + ` ${picStyle ? picStyle : null}` + ` ${textDirection ? textDirection : null}` + ` ${pictureSize ? pictureSize : null}`;
-    return _className;
-  }),
+  classNames:['picture-wrapper'],
 
   actions: {
     /**
@@ -27,13 +18,15 @@ export default Component.extend({
     },
   },
 
-  didInsertElement() {
-    if (this.element) {
-      const showStyle = get(this, 'node.showStyle');
+  picNewMasonry(){
+    const element = this.element.getElementsByClassName('picture-layout')[0];
+    this.newMasonry = new Masonry(element);
+  },
 
-      if (showStyle == 'pinterest') {
-        this.newMasonry = new Masonry(this.element);
-      }
+  didInsertElement() {
+    const showStyle = get(this, 'node.showStyle');
+    if (showStyle == 'pinterest') {
+      scheduleOnce('afterRender',this,'picNewMasonry');
     }
   },
 
