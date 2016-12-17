f5f4a4547bccff2cae4bd046ffa778491bc633cc
shengxx <janisesheng@cform.io>
2016-12-17 13:56:39 +0800

    奖励，抽奖的 服务条款，默认为选中状态

--- addon/modules/services/disclaimer.js
+++ addon/modules/services/disclaimer.js
@@ -2,6 +2,6 @@ import Service from 'ember-service';
 
 export default Service.extend({
 
-  disclaimer:false,
+  disclaimer:true,
 
 });
--- addon/modules/ui-addon/disclaimer/component.js
+++ addon/modules/ui-addon/disclaimer/component.js
@@ -13,7 +13,7 @@ export default Component.extend({
 
 
   isShowException : false,
-  checked:false,
+  checked:true,
 
   actions:{
     isShowException() {
