2fcdeaf07cf458b025eda136fc52246e37ce6bd2
Albert Yu <dev.yufan@gmail.com>
2016-12-20 12:26:14 +0800

    Set accept attribute for mobile devices

--- addon/modules/ui-question/file-upload/component.js
+++ addon/modules/ui-question/file-upload/component.js
@@ -3,13 +3,17 @@ import layout from './template';
 import computed, { reads } from 'ember-computed';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
+import device from 'device';
 
 export default Component.extend({
   layout,
   classNames:['ui-file-upload'],
-
   attributeBindings: ['data-render-id'],
   'data-render-id': reads('option.renderId'),
+
+  accept: computed(function() {
+    return device.mobile() ? 'image/*' : null;
+  }).readOnly(),
 
   icon: computed('option.value', 'option.icon', function () {
     return get(this, 'option.value') ? 'refresh' : get(this, 'option.icon');
@@ -37,15 +41,14 @@ export default Component.extend({
     handleOptionClick(e){
       //如果不能上传图片,就阻止input的默认事件
       !this.handleEvents.handleOptionClick(get(this, 'option'), get(this, 'node'))
-      &&  e.preventDefault();
+        && e.preventDefault();
     },
 
     handleOptionInput(e){
       const data = e.currentTarget.files;
-      set(this,'progress','99');
-      this.handleEvents.handleOptionInput(data, get(this, 'option'), get(this, 'node')).then(()=>{
-        set(this,'progress','100');
-      });
+      set(this, 'progress', '99');
+      this.handleEvents.handleOptionInput(data, get(this, 'option'), get(this, 'node'))
+        .then(() => set(this, 'progress', '100'));
     },
   }
 
