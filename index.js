c2156cca6a467cdf9c660cb6b10452460e1e2615
shengxx <janisesheng@cform.io>
2016-12-20 23:37:20 +0800

    更新 mobile scroll 到3.0版本

--- index.js
+++ index.js
@@ -88,9 +88,9 @@ module.exports = {
     app.import(`./vendor/shims/swiper.js`,
                { outputFile: 'assets/swiper.js' });
 
-    app.import(`./vendor/mobiscroll/css/mobiscroll.custom-3.0.0-beta6.min.css`,
+    app.import(`./vendor/mobiscroll/css/mobiscroll.custom-3.0.0.min.css`,
                { outputFile: 'assets/mobiscroll.css' });
-    app.import(`./vendor/mobiscroll/js/mobiscroll.custom-3.0.0-beta6.min.js`,
+    app.import(`./vendor/mobiscroll/js/mobiscroll.custom-3.0.0.min.js`,
                { outputFile: 'assets/mobiscroll.js' });
     app.import(`./vendor/shims/mobiscroll.js`, { outputFile: 'assets/mobiscroll.js' });
   }
