b28570fa92f4055e7c2fe72eaae5cc284e3163aa
shengxx <janisesheng@cform.io>
2016-12-23 00:02:54 +0800

    particle 在安卓和ie 情况下 不加载 。 并解决矩阵题宽度的问题

--- addon/modules/helpers/particle.js
+++ addon/modules/helpers/particle.js
@@ -1,8 +1,11 @@
 import Helper from 'ember-helper'
 import { htmlSafe } from 'ember-string'
+import device from 'device';
 
 export const particle = (argv, {size = 'm'}) => {
-  return htmlSafe(`<div class="particle-wrapper ${size}">
+  return device.android() || device.windows()
+    ? ''
+    : htmlSafe(`<div class="particle-wrapper ${size}">
             <ul class="particle-container">
               <li class="particle-item"></li>
               <li class="particle-item"></li>
