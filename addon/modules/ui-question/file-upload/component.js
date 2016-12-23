dfc5bf733f9c7d9e9bb91fcac9149c04b428a399
shengxx <janisesheng@cform.io>
2016-12-23 14:36:17 +0800

    上传题 上传同一张图片的时候 版本 0.0.85

--- addon/modules/ui-question/file-upload/component.js
+++ addon/modules/ui-question/file-upload/component.js
@@ -1,17 +1,17 @@
 import Component from 'ember-component';
 import layout from './template';
-import computed, { reads } from 'ember-computed';
+import computed, {reads} from 'ember-computed';
 import get from 'ember-metal/get';
 import set from 'ember-metal/set';
 import device from 'device';
 
 export default Component.extend({
   layout,
-  classNames:['ui-file-upload'],
+  classNames: ['ui-file-upload'],
   attributeBindings: ['data-render-id'],
   'data-render-id': reads('option.renderId'),
 
-  accept: computed(function() {
+  accept: computed(function () {
     return device.mobile() ? 'image/*' : null;
   }).readOnly(),
 
@@ -33,15 +33,16 @@ export default Component.extend({
 
   progress: 0,
 
-  actions:{
+  actions: {
     handleOptionRemove(){
       this.handleEvents.handleOptionInput('', get(this, 'option'), get(this, 'node'));
+      this.element.getElementsByTagName('input')[0].value = '';
     },
 
     handleOptionClick(e){
       //如果不能上传图片,就阻止input的默认事件
       !this.handleEvents.handleOptionClick(get(this, 'option'), get(this, 'node'))
-        && e.preventDefault();
+      && e.preventDefault();
     },
 
     handleOptionInput(e){
@@ -52,4 +53,4 @@ export default Component.extend({
     },
   }
 
-}).reopenClass({ positionalParams: ['node', 'option', 'handleEvents'] });
+}).reopenClass({positionalParams: ['node', 'option', 'handleEvents']});
