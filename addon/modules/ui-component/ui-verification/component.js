cae1d45a9179f121ca2c546e30eb40e5ca5baea1
shengxx <janisesheng@cform.io>
2017-03-13 18:45:09 +0800

    版本 0.1.58

--- addon/modules/ui-component/ui-verification/component.js
+++ addon/modules/ui-component/ui-verification/component.js
@@ -12,6 +12,12 @@ export default Component.extend({
   size: '64px',
   viewBox: '40',
 
+  _icon: computed('verificationType', function () {
+    const type = get(this, 'verificationType');
+    if(type == 'white_list') return 'password';
+    return type;
+  }),
+
   unless_pass_white: computed('verificationType', function () {
     const type = get(this, 'verificationType');
     return ['password','white_list'].indexOf(type) < 0
@@ -22,7 +28,11 @@ export default Component.extend({
     if(type == 'password') return '请输入密码';
     if(type == 'white_list') return '请输入待验证内容';
     return '请输入手机号';
+  }),
 
+  _pass_white: computed('verificationType', function () {
+    const type = get(this, 'verificationType');
+    return ['password','white_list'].indexOf(type) > -1;
   }),
 
 })
