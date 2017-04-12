c007dda021e819a75fab7696a8a6804187ac0046
fenyiwudian <laiq@cform.io>
2017-04-12 12:50:58 +0800

    refactor cascade

--- addon/modules/ui-component/mobi-select/component.js
+++ addon/modules/ui-component/mobi-select/component.js
@@ -15,18 +15,30 @@ export default Component.extend({
   selectInit(){
     initSelect(this.element, {
       onSet: (event, inst) => {
-        get(this, 'handleSelect') && get(this, 'handleSelect')({
-          resultList: inst.getVal(),
-          list: get(this, 'list'),
-          trigger: get(this, 'option'),
-        }, get(this, 'node'));
+        const result = get(this, 'handleSelect') && get(this, 'handleSelect')({
+            resultList: inst.getVal(),
+            list: get(this, 'list'),
+            group: get(this, 'group'),
+          }, get(this, 'node'));
+        // 操作被驳回
+        if (!result) {
+          // 需要恢复到上层数据的状态
+          // 获得上层数据选中的值列表
+          const values = get(this, 'list').reduce((holder, item) => {
+            if (get(item, 'selected')) {
+              holder.push(get(item, 'text'));
+            }
+            return holder;
+          }, []);
+          // 恢复状态
+          inst.setVal(values, true, false);
+        }
       }
     })
   },
 
   didInsertElement(){
     scheduleOnce('afterRender', this, 'selectInit');
-    console.log(get(this, 'multiple'));
   },
 
 });
