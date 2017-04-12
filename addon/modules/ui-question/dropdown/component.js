8b5714a0bf30e3b179eb06e8b50a4a07d8918179
fenyiwudian <laiq@cform.io>
2017-04-12 17:53:24 +0800

    enable multiselect in menuNode

--- addon/modules/ui-question/dropdown/component.js
+++ addon/modules/ui-question/dropdown/component.js
@@ -1,31 +1,10 @@
 import Component from 'ember-component';
 import layout from './template';
-import get from 'ember-metal/get';
 import { reads } from 'ember-computed';
-import { mobiInitTreeList } from '../../lib/mobile-factory';
 
 export default Component.extend({
   layout,
   classNames:['dropdown-layout'],
   attributeBindings:['data-render-id'],
   'data-render-id': reads('node.renderId'),
-
-  didRender(){
-    const input = this.element.getElementsByClassName('dropdown-list')[0];
-
-    mobiInitTreeList(input, {
-      placeholder: get(this, 'node.placeholder'),
-
-      onSet: event => {
-        this.handleEvents.handleQuestionInput(event.valueText, get(this,'node'));
-        input.previousElementSibling.value = get(this, 'node.value');
-      },
-
-      onInit: () => {
-        const mobiInput = input.previousElementSibling;
-        mobiInput.value = get(this, 'node.value');
-        mobiInput.setAttribute('class', 'ui-menu');
-      }
-    });
-  }
 }).reopenClass({ positionalParams: ['node','handleEvents']});
