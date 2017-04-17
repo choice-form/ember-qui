69667c5450825a59152d8d8c138a651085a0e956
Wester <wester@cform.io>
2017-04-17 18:09:51 +0800

    flatten-matrix part2

--- addon/modules/ui-question/flatten-matrix/component.js
+++ addon/modules/ui-question/flatten-matrix/component.js
@@ -1,5 +1,6 @@
 import Component from 'ember-component';
 import layout from './template';
+import get from 'ember-metal/get';
 import set, {setProperties} from 'ember-metal/set';
 import {scheduleOnce} from 'ember-runloop';
 import {gt, and, reads} from 'ember-computed';
@@ -18,7 +19,27 @@ export default Component.extend({
     device.desktop() && window.addEventListener('resize', () => {
       set(this, 'clientWidth', window.innerWidth);
     });
-  }
+  },
+
+  actions: {
+
+    handleOptionClick(option, e){
+      !this.handleEvents.handleOptionClick(option, get(this, 'node'))
+      && e.preventDefault();
+    },
+
+    handleOptionInput(e){
+      const value = e.currentTarget.value;
+      this.handleEvents.handleOptionInput(value, get(this, 'option'), get(this, 'node'));
+    },
+
+    handleOptionInputForTextarea(e){
+      const value = e.currentTarget.value;
+      this.handleEvents.handleOptionInput(value, get(this, 'option'), get(this, 'node'));
+      e.currentTarget.style.height = e.currentTarget.scrollHeight + 2 + 'px';
+    },
+
+  },
 
 
 }).reopenClass({positionalParams: ['node', 'handleEvents']});
