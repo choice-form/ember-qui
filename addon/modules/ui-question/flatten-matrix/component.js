bf1b6ff221afa222a9c0b597fc78109ab0089433
fenyiwudian <laiq@cform.io>
2017-04-17 15:55:00 +0800

    matrix width detect and toggle display mode

--- addon/modules/ui-question/flatten-matrix/component.js
+++ addon/modules/ui-question/flatten-matrix/component.js
@@ -1,6 +1,6 @@
 import Component from 'ember-component';
 import layout from './template';
-import {setProperties} from 'ember-metal/set';
+import set, {setProperties} from 'ember-metal/set';
 import {scheduleOnce} from 'ember-runloop';
 import {gt, and, reads} from 'ember-computed';
 
@@ -10,6 +10,15 @@ export default Component.extend({
 
   attributeBindings: ['data-render-id'],
   'data-render-id': reads('node.renderId'),
+
+  clientWidth: window.innerWidth,
+
+  init(){
+    this._super(...arguments);
+    device.desktop() && window.addEventListener('resize', () => {
+      set(this, 'clientWidth', window.innerWidth);
+    });
+  }
 
 
 }).reopenClass({positionalParams: ['node', 'handleEvents']});
