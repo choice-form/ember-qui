73c345d9cbf0960421d8666ab49f1f10f61522ce
fenyiwudian <laiq@cform.io>
2017-04-28 22:16:03 +0800

    receive video url and poster

--- addon/modules/ui-question/-node/constants.js
+++ addon/modules/ui-question/-node/constants.js
@@ -1,8 +1,9 @@
 export const HEADER_META = [
-  'title', 'description', 'images', 'asterisks', 'number', 'quesType', 'imgScale', 'imgTransTime', 'imgTransType'
+  'title', 'description', 'images', 'asterisks', 'number', 'quesType',
+  'imgScale', 'imgTransTime', 'imgTransType', 'autoPlay', 'video'
 ];
 
 export const SPECIAL_TYPES = [
   'slide', 'dropdown', 'region', 'location', 'matrix', 'intro-page', 'end-page', 'flatten-matrix',
-  'verification', 'ranking', 'weight', 'picture-choice','gender','lottery', 'cascade',
+  'verification', 'ranking', 'weight', 'picture-choice', 'gender', 'lottery', 'cascade',
 ];
