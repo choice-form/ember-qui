9c659b3d7f795a5d72e829fcb0e820ca60c1eb27
fenyiwudian <laiq@cform.io>
2017-05-04 14:32:53 +0800

    ui-video

---
+++ addon/modules/ui-component/ui-video/component.js
@@ -0,0 +1,19 @@
+import Component from 'ember-component';
+import layout from './template';
+
+export default Component.extend({
+  layout,
+  classNames: ['ui-video-container'],
+  didInsertElement(){
+    this._super(...arguments);
+    const video = this.element.querySelector('video');
+    video.setAttribute('src', this.url);
+    video.setAttribute('poster', this.poster);
+    const cover = this.element.querySelector('.error-cover');
+    plyr.setup(this.element);
+    video.addEventListener('error', () => {
+      cover.style.display = 'block';
+      this.element.style.pointerEvents = 'none';
+    });
+  }
+})
