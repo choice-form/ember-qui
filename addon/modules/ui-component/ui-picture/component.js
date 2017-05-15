4f60d949862971ec57826f440964832a405a88fc
Wester <wester@cform.io>
2017-05-15 18:08:41 +0800

    picStyle Fix

--- addon/modules/ui-component/ui-picture/component.js
+++ addon/modules/ui-component/ui-picture/component.js
@@ -1,10 +1,18 @@
 import Component from 'ember-component';
 import layout from './template';
-import {reads} from 'ember-computed';
+import get from 'ember-metal/get';
+import computed, { reads } from 'ember-computed';
 
 export default Component.extend({
   layout,
   classNames: ['ui-picture'],
   attributeBindings: ['data-render-id'],
   'data-render-id': reads('renderId'),
+
+  size: computed('picStyle', function () {
+    if (get(this, 'picStyle') === 'superscript') {
+      return 'm'
+    }
+    return 's'
+  }),
 })
