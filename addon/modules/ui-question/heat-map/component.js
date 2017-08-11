2cadeec3490afc1aa9274c4b948b6b8552b61872
fenyiwudian <laiq@cform.io>
2017-08-11 13:32:55 +0800

    spot render and click handler

--- addon/modules/ui-question/heat-map/component.js
+++ addon/modules/ui-question/heat-map/component.js
@@ -1,25 +1,65 @@
 import Component from 'ember-component';
 import layout from './template';
-import computed, { filterBy, gt } from 'ember-computed';
-import get from 'ember-metal/get';
-import set from 'ember-metal/set';
-import Masonry from 'masonry';
-import { scheduleOnce } from 'ember-runloop';
 
 export default Component.extend({
   layout,
   classNames:['picture-wrapper'],
-
+  reminder: 1,
+  normals: null,
+  others: null,
+  spotList: null,
   init(){
     this._super(...arguments);
+    const normals = [];
+    const others = [];
+    this.node.options.forEach((item) => {
+      if(item.isOther){
+        others.push(item);
+      }else{
+        normals.push(item);
+      }
+    });
+    this.normals = normals;
+    this.others = others;
   },
+
+  didInsertElement(){
+    setTimeout(() => {
+      this.maintainSpot();
+    }, 250);
+
+  },
+
+  maintainSpot(){
+    const $heatMap = $(this.element).find('.heat-map');
+    console.log($heatMap.width());
+    const ratio = $heatMap.width() / this.node.mapImgWidth;
+    const spotList = this.normals.map((item) => {
+      const x = item.x * ratio;
+      const y = item.y * ratio;
+      const height = item.height * ratio;
+      const width = item.width * ratio;
+      return {
+        origin: item,
+        color: item.color,
+        text: item.text,
+        x,
+        y,
+        height,
+        width,
+        style: `left:${x}px;top:${y}px;width:${width}px;height:${height}px;color:${item.color};border-color:${item.color}`
+      }
+    });
+    this.set('spotList', spotList);
+  },
+
 
   actions: {
     /**
      * click事件
      */
-    handleOptionClick(option, e){
-      !this.handleEvents.handleOptionClick(option, get(this, 'node'))
+    handleSpotClick(spot, e){
+      !this.handleEvents.handleOptionClick(spot.origin, this.node)
       && e.preventDefault();
     },
   },
