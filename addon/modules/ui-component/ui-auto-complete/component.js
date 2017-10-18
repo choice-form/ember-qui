a33c94787c57930a5720592df31d540756eb30d0
fenyiwudian <laiq@cform.io>
2017-10-18 19:54:24 +0800

    separated complete list

--- addon/modules/ui-component/ui-auto-complete/component.js
+++ addon/modules/ui-component/ui-auto-complete/component.js
@@ -1,15 +1,11 @@
 import Component from 'ember-component';
 import layout from './template';
-import computed, {reads} from 'ember-computed';
 import {htmlSafe} from 'ember-string';
 
 export default Component.extend({
   layout,
   classNames: ['ui-auto-complete'],
-  hasResult: computed('results', function () {
-    return this.get('results.length') > 0;
-  }),
-  results: [],
+  $dropContainer: null,
   actions: {
     ipTextarea(e){
       if (e.isTrigger) {
@@ -19,23 +15,26 @@ export default Component.extend({
       }
       this.oninput(e);
     },
-    ckOption(e){
-      const value = e.target.textContent;
-      console.log(value);
-      const $textarea = $(this.element).find('textarea');
-      $textarea.val(value).trigger('input');
-      this.set('results', []);
-    },
   },
 
   init(){
     this._super(...arguments);
     this.captureMousedown = this.captureMousedown.bind(this);
+    this.ckOption = this.ckOption.bind(this);
   },
 
   matchText(value, name, triggers){
     return name.toLowerCase().indexOf(value) == 0 ||
-    triggers.some(trigger => trigger.toLowerCase().indexOf(value) == 0);
+      triggers.some(trigger => trigger.toLowerCase().indexOf(value) == 0);
+  },
+
+  ckOption(e){
+    if ($(e.target).hasClass('auto-text')) {
+      const value = e.target.textContent;
+      const $textarea = $(this.element).find('textarea');
+      $textarea.val(value).trigger('input');
+      this.hideDrop();
+    }
   },
 
 
@@ -46,28 +45,74 @@ export default Component.extend({
       results = this.completeGroups.reduce((rs, {name, triggers}) => {
         // 拼音输入法下面输入时字母间可能有空格或'号
         const matched = this.matchText(value, name, triggers) ||
-            this.matchText(value.replace(/['\s]/g, ''), name, triggers);
-        matched && rs.push(name);
+          this.matchText(value.replace(/['\s]/g, ''), name, triggers);
+        matched && (rs += `<div class="auto-text">${name}</div>`);
         return rs;
-      }, []);
+      }, '');
+      this.showDrop(results);
+    } else {
+      this.hideDrop();
     }
 
-    this.set('results', results);
+  },
+
+  getDropStyle(){
+    const rect = this.element.getBoundingClientRect();
+    const style = {
+      left: rect.left,
+      width: rect.width,
+    };
+    const height = this.$dropContainer.height();
+    console.log(height + 60, window.innerHeight - rect.bottom);
+
+    if (height + 60 >= window.innerHeight - rect.bottom) {
+      style.bottom = rect.bottom + rect.height;
+    } else {
+      style.top = rect.bottom;
+    }
+    return Object.keys(style).reduce((rs, key) => {
+      return rs + `${key}:${style[key]}px;`
+    }, '');
+  },
+
+  showDrop(results){
+    this.$dropContainer.empty().append(results)
+      .attr('style', this.getDropStyle())
+      .show();
+
+  },
+
+  hideDrop(){
+    this.$dropContainer.hide().empty();
+  },
+
+
+  initDrop(){
+    let $dropContainer = $('body > .complete-list');
+    if ($dropContainer.length === 0) {
+      $dropContainer = $('<div class="complete-list"></div>').hide();
+      $(document.body).append($dropContainer);
+    }
+    this.$dropContainer = $dropContainer;
+
+    this.$dropContainer.on('click', this.ckOption);
   },
 
   captureMousedown(e){
-    if(e.target === this.element || this.element.contains(e.target)){
+    if (e.target === this.element || this.element.contains(e.target)) {
       return;
     }
-    this.set('results', []);
+    this.hideDrop();
   },
 
   didInsertElement(){
     window.addEventListener('click', this.captureMousedown, true);
+    this.initDrop();
   },
 
   willDestroyElement(){
     window.removeEventListener('click', this.captureMousedown, true);
+    this.$dropContainer.off('click', this.ckOption);
   },
 
 });
