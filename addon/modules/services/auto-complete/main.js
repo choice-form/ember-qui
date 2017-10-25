cf2cf3913cc34b9c723d118b6836f6811c9f3900
fenyiwudian <laiq@cform.io>
2017-10-25 21:14:36 +0800

    complete menu in component

--- addon/modules/services/auto-complete/main.js
+++ addon/modules/services/auto-complete/main.js
@@ -1,88 +1,5 @@
 import {getPinyin} from './pinyin';
 
-let $completeMenu = null;
-
-let menu = null;
-
-let owner = null;
-
-let textarea = null;
-
-let lastResizeStyle = '';
-
-let scrollHandling = false;
-
-let existed = null;
-
-
-/**
- * 判断是否为移动端
- * @returns {*|jQuery}
- */
-let isMobile = () => {
-  const result = $('html').hasClass('mobile');
-  isMobile = () => {
-    return result;
-  };
-  return result;
-};
-
-
-/**
- * 处理滚动
- */
-const handleScroll = () => {
-  if (!scrollHandling) {
-    window.requestAnimationFrame(() => {
-      $completeMenu.attr('style', getStyle());
-      scrollHandling = false;
-    })
-  }
-  scrollHandling = true;
-};
-
-/**
- * 处理窗口尺寸变化事件
- */
-const handleResize = () => {
-  const style = getStyle();
-  if (style === lastResizeStyle) {
-    lastResizeStyle = '';
-  } else {
-    $completeMenu.attr('style', style);
-    lastResizeStyle = style;
-    setTimeout(handleResize, 100);
-  }
-
-};
-
-
-/**
- * 处理选择事件
- * @param e
- */
-const handleSelect = (e) => {
-  if ($(e.target).hasClass('complete-option')) {
-    const value = existed.concat(e.target.textContent).join(',');
-    $(textarea).val(value).trigger('input');
-    hide();
-  }
-};
-
-/**
- * 捕捉鼠标落下事件
- * @param e
- */
-const captureMouseDown = (e) => {
-  if (e.target === owner.element
-    || owner.element.contains(e.target)
-    || e.target === menu
-    || menu.contains(e.target)) {
-    return;
-  }
-  hide();
-};
-
 /**
  * 匹配规则
  * @type {object}
@@ -149,11 +66,10 @@ const matchConfig = (valueList, {name, triggers}, existed, rule) => {
  */
 const analyzeValue = (text) => {
   const list = text.split(/[,，]/g);
+  let existed = [];
   if (list.length > 1) {
     text = list.pop();
     existed = list;
-  } else {
-    existed = [];
   }
   return {
     value: text.toLowerCase(),
@@ -165,95 +81,23 @@ const analyzeValue = (text) => {
  * 搜索匹配结果
  * @param {string} text 搜索关键字
  * @param {Array} dataSource 搜索元数据
- * @param {string} rule 匹配规则
- * @returns {string}
+ * @param {string} [rule] 匹配规则
+ * @returns {Object}
  */
-const searchResults = (text, dataSource, rule) => {
+export const searchResult = (text, dataSource, rule) => {
   const {value, existed} = analyzeValue(text);
+  let result = [];
   if (value) {
-    return dataSource.reduce((rs, config) => {
+    result = dataSource.reduce((rs, config) => {
       // 拼音输入法下面输入时字母间可能有空格或'号
       const matched = matchConfig([value, value.replace(/['\s]/g, '')], config, existed, rule);
-      matched && (rs += `<div class="complete-option">${config.name}</div>`);
+      matched && (rs.push(config.name));
       return rs;
-    }, '');
+    }, []);
   }
-  return '';
-};
-
-
-/**
- * 获得菜单样式
- * @returns {*}
- */
-const getStyle = () => {
-  const rect = owner.element.getBoundingClientRect();
-  const style = {
-    left: rect.left - 8,
-    'max-width': rect.width,
-    top: rect.bottom,
-  };
-  return Object.keys(style).reduce((rs, key) => {
-    return rs + `${key}:${style[key]}px;`
-  }, '');
-};
-
-
-/**
- * 初始化自动提示菜单
- */
-const init = () => {
-  if (!$completeMenu) {
-    $completeMenu = $('<div class="auto-complete-menu"></div>').hide();
-    menu = $completeMenu[0];
-    $(document.body).append($completeMenu);
-    $completeMenu.on('click', handleSelect);
+  return {
+    result,
+    existed,
   }
-};
-
-
-/**
- * 隐藏自动提示菜单
- */
-const hide = () => {
-  $completeMenu.hide().empty();
-  owner = null;
-  textarea = null;
-  window.removeEventListener('mousedown', captureMouseDown, true);
-  isMobile() && window.removeEventListener('resize', handleResize);
-  window.removeEventListener('scroll', handleScroll);
-};
-
-
-let taskId = -1;
-
-
-/**
- * 召唤自动提示菜单
- * @param {Component} component 触发源组件
- * @param {HTMLInputElement} inputElem 触发源输入框
- * @param {Array} dataSource 内容匹配元数据
- * @param {string} rule 匹配规则 partial: 任意部分匹配, start:从头开始部分匹配, full:全部匹配
- */
-export const autoComplete = (component, inputElem, dataSource, rule) => {
-
-  clearTimeout(taskId);
-  taskId = setTimeout(() => {
-    init();
-    const result = searchResults(inputElem.value, dataSource, rule);
-    if (result) {
-      owner = component;
-      textarea = inputElem;
-      $completeMenu.empty().append(result)
-        .attr('style', getStyle())
-        .show();
-      window.addEventListener('mousedown', captureMouseDown, true);
-      window.addEventListener('scroll', handleScroll);
-      isMobile() && window.addEventListener('resize', handleResize);
-    } else {
-      $completeMenu.hide();
-    }
-  }, 250);
-
 };
 
--- addon/modules/ui-component/ui-auto-complete/component.js
+++ addon/modules/ui-component/ui-auto-complete/component.js
@@ -1,21 +1,63 @@
 import Component from 'ember-component';
 import layout from './template';
 import {htmlSafe} from 'ember-string';
-import {autoComplete} from '../../services/auto-complete/main';
+import {notEmpty} from 'ember-computed';
+import {searchResult} from '../../services/auto-complete/main';
 
 export default Component.extend({
   layout,
   classNames: ['ui-auto-complete'],
-  $dropContainer: null,
+  result: [],
+  hasResult: notEmpty('result').readOnly(),
+  $textarea: null,
   actions: {
     ipTextarea(e){
       if (e.isTrigger) {
         e.currentTarget = e.target;
       } else {
-        autoComplete(this, e.target, this.completeGroups);
+        this.searchBy(e.target.value);
       }
       this.oninput(e);
     },
+    clickOption(e){
+      const value = this.existed.concat(e.target.textContent).join(',');
+      this.$textarea.val(value).trigger('input');
+      this.closeMenu();
+    }
   },
+
+  init(){
+    this._super(...arguments);
+    this.captureMouseDown = this.captureMouseDown.bind(this);
+  },
+
+  captureMouseDown(e){
+    if(e.target !== this.element && !this.element.contains(e.target)){
+      this.closeMenu();
+    }
+  },
+
+  searchBy(keyword){
+    clearTimeout(this.taskId);
+    this.taskId = setTimeout(()=>{
+      const {result, existed} = searchResult(keyword, this.completeGroups);
+      this.set('result', result);
+      this.existed = existed;
+    }, 250);
+  },
+
+  closeMenu(){
+    this.set('result', []);
+    this.existed = [];
+  },
+
+  didInsertElement(){
+    this.$textarea = $(this.element).find('textarea');
+    window.addEventListener('mousedown', this.captureMouseDown, true);
+  },
+
+  willDestroyElement(){
+    window.removeEventListener('mousedown', this.captureMouseDown, true);
+  }
 
 });
