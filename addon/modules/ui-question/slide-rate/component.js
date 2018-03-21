c98730ef12da3822186a06ab7b867ce7f93f7d74
李旭 <leexu@cform.io>
2018-03-21 16:10:49 +0800

    finish logical of slide-rate

--- addon/modules/ui-question/slide-rate/component.js
+++ addon/modules/ui-question/slide-rate/component.js
@@ -1,9 +1,62 @@
 import Component from 'ember-component';
 import layout from './template';
+import inject from 'ember-service/inject';
+import computed from 'ember-computed';
+import set from 'ember-metal/set';
+import Swiper from 'swiper';
 
 export default Component.extend({
   layout,
   tagName:'',
+  iconService: inject("icon-loader"),
 
+  rateOptions: computed('options.@each.value', function() {
+    const result = this.get('options').reduce((acc, option) => {
+      acc[option.value] ? acc[option.value].push(option) : acc[option.value] = [option];
+      return acc;
+    }, {});
 
-}).reopenClass({ positionalParams: ['node','handleEvents']});
+    return this.get('node.rateOptions').map(rateOption => {
+      set(rateOption, 'result', result[rateOption.value] || []);
+      return rateOption;
+    });
+  }),
+
+  currentOption: computed(function() {
+    return this.get('options')[0];
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    this.get('rateOptions').forEach(rateOption => {
+      this.get('iconService').getIconByUrl(rateOption.url, rateOption.url)
+        .then(icon => set(rateOption, 'svg', icon.outerHTML));
+    });
+
+    const swiper = new Swiper('.swiper-container', {
+      allowSwipeToNext: false,
+      pagination: '.swiper-pagination',
+      onSlideChangeEnd: (swiper) => {
+        const option = this.get('options')[swiper.activeIndex];
+        set(this, 'currentOption', option);
+        option.value ? swiper.unlockSwipeToNext() : swiper.lockSwipeToNext();
+      },
+    });
+
+    this.set('swiper', swiper);
+  },
+
+  actions: {
+    handleOptionInput(value) {
+      const swiper = this.get('swiper');
+
+      const option = this.get('options')[swiper.activeIndex];
+      this.handleEvents.handleOptionInput(value, option, this.get('node'));
+
+      swiper.unlockSwipeToNext();
+      swiper.slideNext();
+      swiper.lockSwipeToNext();
+    }
+  }
+}).reopenClass({positionalParams: ['node','handleEvents']});
