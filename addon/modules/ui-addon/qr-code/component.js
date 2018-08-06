69d444b397abb855614e2628dc8d2e8fa7a3a620
fenyiwudian <laiq@cform.io>
2018-08-06 15:11:17 +0800

    add prop: showText for qr-code component

--- addon/modules/ui-addon/qr-code/component.js
+++ addon/modules/ui-addon/qr-code/component.js
@@ -2,23 +2,23 @@ import Component from 'ember-component';
 import set from 'ember-metal/set';
 import get from 'ember-metal/get';
 import QRCode from 'qrcode';
-import computed, { reads } from 'ember-computed';
+import computed, {reads} from 'ember-computed';
 
 export default Component.extend({
   tagName: 'canvas',
   classNames: ['qr-code'],
   attributeBindings: ['width', 'height'],
-  size:360,
+  size: 360,
   width: reads('size'),
   height: computed('size', 'padding', function () {
 
-    return get(this,'size')+ get(this, 'padding') / 4 + 16
+    return get(this, 'size') + get(this, 'padding') / 4 + 16
   }),
   light: 'white',
   dark: 'black',
   ctx: null,
   data: '',
-  padding:60,
+  padding: 60,
 
   didInsertElement() {
     HTMLElement.prototype.__defineGetter__("currentStyle", function () {
@@ -30,7 +30,7 @@ export default Component.extend({
     set(this, 'light', backgroundColor);
     set(this, 'dark', color);
 
-    set(this,'ctx', this.element.getContext('2d'));
+    set(this, 'ctx', this.element.getContext('2d'));
 
     return this.draw();
   },
@@ -57,25 +57,30 @@ export default Component.extend({
     const cheight = height / size;
     const pad = 0;
 
-    function cx(x) {return x * cwidth;}
-    function cy(y) {return y * cheight;}
+    function cx(x) {
+      return x * cwidth;
+    }
+
+    function cy(y) {
+      return y * cheight;
+    }
 
     ctx.fillStyle = document.body.currentStyle.color;
 
     ctx.font = "16px Arial";
     //设置字体填充颜色
-    ctx.textAlign="center";
-    ctx.fillText(data, get(this, 'size') / 2,  (get(this, 'height') - get(this, 'padding') / 2));
+    ctx.textAlign = "center";
+    if (this.showText) {
+      ctx.fillText(data, get(this, 'size') / 2, (get(this, 'height') - get(this, 'padding') / 2));
+    }
     ctx.fillStyle = get(this, 'dark');
     const offestX = (get(this, 'size') - width) / 2;
     const offestY = (get(this, 'size') - height) / 2;
     ctx.translate(offestX, offestY);
     for (let row = 0; row < size; ++row)
       for (let col = 0; col < size; ++col)
-        if (qr.isDark(row, col)){
+        if (qr.isDark(row, col)) {
           ctx.fillRect(cx(row) + pad, cy(col) + pad, cwidth - pad, cheight - pad);
         }
-
-
   }
 });
